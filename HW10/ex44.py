cbaf9fd07c76908b3acf0799c586c6f09db34e4c
Vlad-Alesin <120244703+Vlad-Alesin@users.noreply.github.com>
2023-04-14 15:22:06 +0300

    дз 10

---
+++ HW10/ex44.py
@@ -0,0 +1,19 @@
+"""В ячейке ниже представлен код генерирующий DataFrame, 
+которая состоит всего из 1 столбца. Ваша задача перевести его в one hot вид. 
+Сможете ли вы это сделать без get_dummies
+"""
+
+
+import random
+import pandas as pd
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+
+data.loc[data['whoAmI']== 'robot', 'robot']='1'
+data.loc[data['whoAmI']!= 'robot', 'robot']='0'
+
+print(data)
+
